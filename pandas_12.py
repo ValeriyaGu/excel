5e246e76e37bd33c545ffda36a9beb5ff1aee809
ValeriyaGu <valkiriyalera@gmail.com>
2021-01-05 16:30:19 +0700

    edits

--- pandas_12.py
+++ pandas_12.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import numpy as np
 
-array1 = np.arange(9).reshape((3,3))
+array1 = np.arange(9).reshape((3, 3))
 
-array2 = np.arange(9).reshape((3,3))+6
+array2 = np.arange(9).reshape((3, 3)) + 6
 
-np.concatenate([array1,array2],axis=1)
+np.concatenate([array1, array2], axis=1)
 
-np.concatenate([array1,array2],axis=0)
+np.concatenate([array1, array2], axis=0)
 
-print(np.concatenate([array1,array2],axis=0))
+print(np.concatenate([array1, array2], axis=0))
 
-ser1 = pd.Series(np.random.rand(4), index=[1,2,3,4])
+ser1 = pd.Series(np.random.rand(4), index=[1, 2, 3, 4])
 
-ser2 = pd.Series(np.random.rand(4), index=[5,6,7,8])
+ser2 = pd.Series(np.random.rand(4), index=[5, 6, 7, 8])
 
-print(pd.concat([ser1,ser2], keys=[1,2]))
+print(pd.concat([ser1, ser2], keys=[1, 2]))
--- pandas_13.py
+++ pandas_13.py
@@ -1,22 +1,20 @@
 import pandas as pd
 import numpy as np
 
-dframe = pd.DataFrame({'color': ['white','white','red','red','white'],
- 			   'value': [2,1,3,3,2]})
+dframe = pd.DataFrame({'color': ['white', 'white', 'red', 'red', 'white'],
+                       'value': [2, 1, 3, 3, 2]})
 
 dframe = dframe.drop_duplicates()
 
+frame = pd.DataFrame({'item': ['ball', 'mug', 'pen', 'pencil', 'ashtray'],
+                      'color': ['white', 'rosso', 'verde', 'black', 'yellow'],
+                      'price': [5.56, 4.20, 1.30, 0.56, 2.75]})
 
-
-frame = pd.DataFrame({'item':['ball','mug','pen','pencil','ashtray'],
- 			  'color':['white','rosso','verde','black','yellow'],
-			  'price':[5.56,4.20,1.30,0.56,2.75]})
-              
 print(frame)
 
 newcolors = {
- 	'rosso': 'red',
- 	'verde': 'green'
+    'rosso': 'red',
+    'verde': 'green'
 }
 
 print(frame.replace(newcolors))
